87046f0d32b85bf1d18eb11a1ee2f9331a823326
ahyng <ahyng1@gmail.com>
2024-08-06 22:48:34 +0900

    글쓰기 페이지에 작성자 id 불러오기

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/list', (req, res) => {
 })
 
 app.get('/enter', (req, res) => {
-  res.render('enter.ejs');
+  res.render('enter.ejs', {user : req.session.user? req.session.user : {userid : '로그인 후 이용하세요'}});
 })
 
 app.post('/save', (req, res) => {
